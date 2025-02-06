0e89855a57279c894395a0c4ef6c592f2f2f3f0e
nanako1106 <nanakai626@gmail.com>
2025-02-07 01:15:07 +0900

    スクール詳細ページ途中

---
+++ client/app/schools/components/SchoolHeader.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { School } from "@/types/school";
+import MenuTabs from "./MenuTabs";
+
+export default function SchoolHeader({ school }: { school: School }) {
+  return (
+    <section className="relative w-full h-[250px] bg-gray-100">
+      <div className="flex max-full mx-auto items-center space-x-6 p-4 ">
+        {/* 左側: ロゴ */}
+        <div className="flex items-center justify-center w-24 h-24">
+          <Image
+            src={school.logo || "/defaultLogo.png"}
+            alt={school.name}
+            width={96}
+            height={96}
+            className="object-cover"
+          />
+        </div>
+
+        {/* 右側: スクール情報 */}
+        <div className="flex flex-col">
+          {/* スクール名 */}
+          <h1 className="text-2xl font-bold text-gray-700">{school.name}</h1>
+
+          {/* 星評価 */}
+          <div className="flex">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <span
+                key={index}
+                className={
+                  index < school.rating ? "text-yellow-400" : "text-gray-300"
+                }
+              >
+                ★
+              </span>
+            ))}
+          </div>
+
+          {/* 説明 */}
+          <p className="mt-2 text-gray-600 font-md">{school.description}</p>
+        </div>
+      </div>
+      <div className="absolute bottom-0 w-full">
+        <MenuTabs />
+      </div>
+    </section>
+  );
+}
---
+++ client/app/types/school.ts
@@ -0,0 +1,7 @@
+export type School = {
+  id: string;
+  name: string;
+  rating: number;
+  description: string;
+  logo: string;
+};
