7e2e59a880a7962247a289335fe0c60c6ccaf00f
nanako1106 <nanakai626@gmail.com>
2025-02-25 03:41:32 +0900

    eslintエラー修正

--- client/app/lib/prisma.ts
+++ client/app/lib/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+// ✅ 開発環境 (`dev`) では `global` を利用
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
