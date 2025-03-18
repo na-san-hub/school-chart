9bebe2ce14ab3981a839e748656ac164e521d30a
nanako1106 <nanakai626@gmail.com>
2025-03-18 23:05:53 +0900

    prismaログレベル設定

--- client/app/lib/prisma.ts
+++ client/app/lib/prisma.ts
@@ -2,7 +2,12 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// ✅ 開発環境 (`dev`) では `global` を利用
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+// 開発環境では global を利用し、再接続を避ける
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    // ログレベルをエラーだけに制限してオーバーヘッドを減らす
+    log: ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
