efb16eee951d861073a19f70e7f4947263bb75c5
nanako1106 <nanakai626@gmail.com>
2025-03-19 14:22:30 +0900

    Courseテーブルindex追加

---
+++ client/prisma/migrations/20250319051954_add_search_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "idx_course_delivery_method" ON "course"("delivery_method");
+
+-- CreateIndex
+CREATE INDEX "idx_course_location_prefecture" ON "course"("location_prefecture");
+
+-- CreateIndex
+CREATE INDEX "idx_course_price" ON "course"("price");
