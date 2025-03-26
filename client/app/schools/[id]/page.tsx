fe560992d8cfe28d2608e020741c34d56548507b
nanako1106 <nanakai626@gmail.com>
2025-03-26 18:33:02 +0900

    スクールトップに口コミ追加

--- client/app/schools/[id]/page.tsx
+++ client/app/schools/[id]/page.tsx
@@ -3,6 +3,8 @@ export const dynamic = "force-dynamic";
 import ChartSection from "@/schools/[id]/components/ChartSection";
 import { getSchoolWithCourses, getRadarChartData } from "@/lib/school";
 import SchoolDetail from "@/schools/[id]/components/SchoolDetail";
+import { getReviewsForSchool } from "@/lib/reviews";
+import ReviewCardList from "./components/reviews/ReviewCardList";
 
 export default async function SchoolPage(props: {
   params: Promise<{ id: string }>;
@@ -14,11 +16,14 @@ export default async function SchoolPage(props: {
   const chartData = await getRadarChartData(schoolId);
   const school = await getSchoolWithCourses(schoolId);
 
+  const reviews = await getReviewsForSchool(schoolId);
+
   return (
     <div className="border-t border-t-gray-400 w-full">
       <div className="mt-5 flex flex-wrap max-w-5xl mx-auto justify-between items-center">
         <ChartSection chartData={chartData} />
         <SchoolDetail school={school} />
+        <ReviewCardList reviews={reviews} />
       </div>
     </div>
   );
