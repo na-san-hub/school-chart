2bb860f575bab11def7471fae27415261f2dd2bf
nanako1106 <nanakai626@gmail.com>
2025-04-09 12:10:04 +0900

    エラー修正

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,10 +1,13 @@
 import LoginForm from "./components/LoginForm";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <div className="flex flex-col items-center flex-grow bg-gray-50 justify-center py-14 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full">
-        <LoginForm />
+        <Suspense fallback={<div>読み込み中...</div>}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
